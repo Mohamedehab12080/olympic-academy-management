ad6ff2601deff7caf4ffecb5bd89c15e2efb5c91
mohamed-ehab120 <mohamed.ehab@algoorasystems.com>
2026-05-22 21:44:50 +0300

    initialized

---
+++ src/test/java/bs/olympic/academy/management/ApplicationTests.java
@@ -0,0 +1,13 @@
+package bs.olympic.academy.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
